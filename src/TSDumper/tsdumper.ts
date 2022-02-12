28371a956ef2fe80bd342addff2e6f7dc030f66c
Anthony Iacono <anthonyiacono@gmail.com>
2022-02-12 11:04:17 -0500

    TSDumper: Initial commit
    This dumps the raw AST nodes of the `lib.dom.d.ts` file and outputs into the `output` directory.

---
+++ src/TSDumper/tsdumper.ts
@@ -0,0 +1,36 @@
+import * as fs from "fs";
+const ts = require('typescript');
+
+const inputTypeDefinitions = [
+    'lib.dom.d',
+];
+
+const getCircularReplacer = () => {
+    const seen = new WeakSet();
+    return (key: any, value: any) => {
+        if (typeof value === "object" && value !== null) {
+            if (seen.has(value)) {
+                return;
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
+fs.mkdirSync('output', {recursive: true});
+
+inputTypeDefinitions.forEach(inputTypeDefinition => {
+    const inputPath = `node_modules/typescript/lib/${inputTypeDefinition}.ts`;
+    const outputPath = `output/${inputTypeDefinition}.json`;
+
+    console.log(`Dumping AST for "${inputPath}" to "${outputPath}"...`);
+
+    const node = ts.createSourceFile(
+        'x.ts',
+        fs.readFileSync(inputPath, {encoding:'utf8', flag:'r'}),
+        ts.ScriptTarget.Latest
+    );
+
+    fs.writeFileSync(outputPath, JSON.stringify(node, getCircularReplacer()));
+});
