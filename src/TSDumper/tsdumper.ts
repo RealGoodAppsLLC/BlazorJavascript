3a6e5ebdad411008053b74afd4ba68d5aa72a2c1
Anthony Iacono <anthonyiacono@gmail.com>
2022-02-12 11:10:16 -0500

    TSDumper: Adding interfaces and their "extends" list to the output
    This commit parses interfaces from the type definitions. Currently only the name and the "extends" list are added to the output, and we can add functions and properties in a follow-up.

--- src/TSDumper/tsdumper.ts
+++ src/TSDumper/tsdumper.ts
@@ -45,8 +45,14 @@ interface GlobalVariableInfo {
     constructors: ConstructorInfo[];
 }
 
+interface InterfaceInfo {
+    name: string;
+    extendsList: string[]
+}
+
 interface ParsedInfo {
     globalVariables: GlobalVariableInfo[];
+    interfaces: InterfaceInfo[];
 }
 
 function extractTypeName(typeNode: ts.TypeNode): string {
@@ -120,6 +126,7 @@ inputTypeDefinitions.forEach(inputTypeDefinition => {
 
     const parsedInfo: ParsedInfo = {
         globalVariables: [],
+        interfaces: [],
     };
 
     if (isDebugMode) {
@@ -127,7 +134,33 @@ inputTypeDefinitions.forEach(inputTypeDefinition => {
     }
 
     sourceFile.statements.forEach(statement => {
-        if (ts.isInterfaceDeclaration(statement)) {
+        if (ts.isInterfaceDeclaration(statement)
+            && ts.isIdentifier(statement.name)) {
+            const extendsList: string[] = [];
+
+            if (!!statement.heritageClauses) {
+                statement.heritageClauses.forEach(heritageClause => {
+                    if (heritageClause.token !== SyntaxKind.ExtendsKeyword) {
+                        console.error("Heritage clause detected without extends keyword.");
+                        return;
+                    }
+
+                    heritageClause.types.forEach(type => {
+                        if (!ts.isIdentifier(type.expression)) {
+                            return;
+                        }
+
+                        extendsList.push(type.expression.text);
+                    });
+                });
+            }
+
+            const interfaceInfo: InterfaceInfo = {
+                name: statement.name.text,
+                extendsList: extendsList,
+            };
+
+            parsedInfo.interfaces.push(interfaceInfo);
             return;
         }
 
