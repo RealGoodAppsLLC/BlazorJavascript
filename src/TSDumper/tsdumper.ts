e463a608862d0b647695210b4be5cdb03dcc08a5
Anthony Iacono <anthonyiacono@gmail.com>
2022-02-12 11:06:27 -0500

    TSDumper: Add the list of classes (aka any variable declaration with a prototype)

--- src/TSDumper/tsdumper.ts
+++ src/TSDumper/tsdumper.ts
@@ -1,5 +1,14 @@
 import * as fs from "fs";
-const ts = require('typescript');
+import {
+    Block,
+    InterfaceDeclaration,
+    isTypeReferenceNode,
+    SourceFile,
+    SyntaxKind,
+    VariableDeclaration,
+    VariableStatement
+} from "typescript";
+import * as ts from "typescript";
 
 const inputTypeDefinitions = [
     'lib.dom.d',
@@ -20,17 +29,72 @@ const getCircularReplacer = () => {
 
 fs.mkdirSync('output', {recursive: true});
 
+interface ClassInfo {
+    name: string;
+}
+
+interface ParsedInfo {
+    classes: ClassInfo[];
+    raw: any;
+}
+
 inputTypeDefinitions.forEach(inputTypeDefinition => {
     const inputPath = `node_modules/typescript/lib/${inputTypeDefinition}.ts`;
     const outputPath = `output/${inputTypeDefinition}.json`;
 
     console.log(`Dumping AST for "${inputPath}" to "${outputPath}"...`);
 
-    const node = ts.createSourceFile(
+    const sourceFile: SourceFile = ts.createSourceFile(
         'x.ts',
         fs.readFileSync(inputPath, {encoding:'utf8', flag:'r'}),
         ts.ScriptTarget.Latest
     );
 
-    fs.writeFileSync(outputPath, JSON.stringify(node, getCircularReplacer()));
+    const parsedInfo: ParsedInfo = {
+        classes: [],
+        raw: sourceFile,
+    };
+
+    sourceFile.statements.forEach(statement => {
+        if (ts.isInterfaceDeclaration(statement)) {
+            console.log(`interface decl: ${statement.name.text}`);
+            return;
+        }
+
+        if (ts.isVariableStatement(statement)) {
+            statement.declarationList.declarations.forEach(declaration => {
+                if (!ts.isVariableDeclaration(declaration)
+                    || !declaration.type
+                    || !ts.isTypeLiteralNode(declaration.type)
+                    || !ts.isIdentifier(declaration.name)) {
+                    return;
+                }
+
+                let hasPrototype = false;
+
+                const declarationName = declaration.name.text;
+
+                declaration.type.members.forEach(member => {
+                    if (ts.isPropertySignature(member)
+                        && ts.isIdentifier(member.name)
+                        && member.name.text === "prototype"
+                        && member.type
+                        && ts.isTypeReferenceNode(member.type)
+                        && ts.isIdentifier(member.type.typeName)
+                        && member.type.typeName.text === declarationName) {
+                        hasPrototype = true;
+                        return;
+                    }
+                });
+
+                if (hasPrototype) {
+                    parsedInfo.classes.push({
+                        name: declarationName,
+                    });
+                }
+            });
+        }
+    });
+
+    fs.writeFileSync(outputPath, JSON.stringify(parsedInfo, getCircularReplacer()));
 });
