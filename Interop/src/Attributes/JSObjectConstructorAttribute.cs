7b83e830fe7b0ba459bb78b317e30532b873691a
Anthony Iacono <anthonyiacono@gmail.com>
2022-02-26 14:01:04 -0500

    WIP stylecop

--- Interop/src/Attributes/JSObjectConstructorAttribute.cs
+++ Interop/src/Attributes/JSObjectConstructorAttribute.cs
@@ -1,14 +1,31 @@
+// <copyright file="JSObjectConstructorAttribute.cs" company="Real Good Apps">
+// Copyright (c) Real Good Apps. All rights reserved.
+// </copyright>
+
 using System;
+using RealGoodApps.BlazorJavascript.Interop.BuiltIns;
 
 namespace RealGoodApps.BlazorJavascript.Interop.Attributes
 {
+    /// <summary>
+    /// Attribute used to annotation interfaces that extend <see cref="IJSObject"/> which specifies the type that should
+    /// be constructed when the object factory attempts to return the interface.
+    /// </summary>
     public class JSObjectConstructorAttribute : Attribute
     {
-        public Type Type { get; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JSObjectConstructorAttribute"/> class.
+        /// </summary>
+        /// <param name="type">The type of object that should be constructed for the interface.</param>
         public JSObjectConstructorAttribute(Type type)
         {
-            Type = type;
+            this.Type = type;
         }
+
+        /// <summary>
+        /// Gets the type of object that should be constructed for the interface.
+        /// This must be a type that implements the interface that applies the attribute.
+        /// </summary>
+        public Type Type { get; }
     }
 }
