5cee8665f0978d8acda59196e7127225cd618719
Anthony Iacono <anthonyiacono@gmail.com>
2022-02-26 21:55:13 -0500

    TSDumper: Detect if a parameter is using the `...` (aka dot dot dot token) modifier
    TypeScript function parameters can be annotated with `...` when they are the last parameter for a signature. This allows them to be interpreted as an array of values, where you can effectively pass any number of parameters to the argument and they will be interpreted as an array.
    
    Let's track this modifier so we can take advantage of it when we generate code.

--- TSDumper/src/includes/parameters.ts
+++ TSDumper/src/includes/parameters.ts
@@ -4,12 +4,18 @@ import { extractTypeInfo, TypeInfo } from "./types";
 export interface ParameterInfo {
     name: string;
     isOptional: boolean;
+    isDotDotDot: boolean;
     type: TypeInfo;
 }
 
 const isParameterOptional = (parameterDeclaration: ts.ParameterDeclaration): boolean => {
     return !!parameterDeclaration.questionToken
         && parameterDeclaration.questionToken.kind == ts.SyntaxKind.QuestionToken
+};
+
+const isParameterDotDotDot = (parameterDeclaration: ts.ParameterDeclaration): boolean => {
+    return !!parameterDeclaration.dotDotDotToken
+        && parameterDeclaration.dotDotDotToken.kind == ts.SyntaxKind.DotDotDotToken
 };
 
 export const extractParameters = (member: ts.NodeArray<ts.ParameterDeclaration>): ParameterInfo[] => {
@@ -23,6 +29,7 @@ export const extractParameters = (member: ts.NodeArray<ts.ParameterDeclaration>)
         parameters.push({
             name: parameter.name.text,
             isOptional: isParameterOptional(parameter),
+            isDotDotDot: isParameterDotDotDot(parameter),
             type: extractTypeInfo(parameter.type),
         });
     });
